cbbe15a7a7d5d72ef11cdd6b46f99d79b7a1fc58
Edvard Jinder <edvard.jinder@intromedia.se>
2025-09-28 14:16:27 +0200

    moved test params to env

--- tests/OnePassword.Cli.Sdk.Tests/GetItemTests.cs
+++ tests/OnePassword.Cli.Sdk.Tests/GetItemTests.cs
@@ -2,19 +2,19 @@
 
 namespace OnePassword.Cli.Sdk.Tests;
 
+
 public class GetItemTests
 {
 
-    [Theory]
-    [InlineData("H5GH4RDY6BFHVDJ2JO32OOWEOI", "qwwh4velrmj6p4snz6je3bjpmy", "iglqlo2pjgsvroc3n6vpjwlpm4", "username", "someusername")]
-    public async Task ShouldRetrieveField(
-        string accountId,
-        string vaultId,
-        string itemId,
-        string field,
-        string expectedValue
-        )
+    [Fact]
+    public async Task ShouldRetrieveField()
     {
+        var accountId = Environment.GetEnvironmentVariable("OP_ACCOUNT_ID") ?? throw new InvalidOperationException("OP_ACCOUNT_ID environment variable is not set.");
+        var vaultId = Environment.GetEnvironmentVariable("OP_VAULT_ID") ?? throw new InvalidOperationException("OP_VAULT_ID environment variable is not set.");
+        var itemId = Environment.GetEnvironmentVariable("OP_ITEM_ID") ?? throw new InvalidOperationException("OP_ITEM_ID environment variable is not set.");
+        var fieldName = Environment.GetEnvironmentVariable("OP_FIELD") ?? throw new InvalidOperationException("OP_FIELD environment variable is not set.");
+        var expectedFieldValue = Environment.GetEnvironmentVariable("OP_FIELD_VALUE") ?? throw new InvalidOperationException("OP_FIELD_VALUE environment variable is not set.");
+
         var serviceCollection = new ServiceCollection();
         serviceCollection.TryAdd1PasswordCliServices();
 
@@ -22,8 +22,8 @@ public class GetItemTests
 
         var client = serviceProvider.GetRequiredService<IOnePasswordClient>();
 
-        var value = await client.GetFieldAsync(accountId, vaultId, itemId, field, null, default);
+        var value = await client.GetFieldAsync(accountId, vaultId, itemId, fieldName, null, default);
 
-        Assert.Equal(expectedValue, value);
+        Assert.Equal(expectedFieldValue, value);
     }
 }
